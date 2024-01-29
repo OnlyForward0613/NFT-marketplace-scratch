db29c8a35a0ba3d0a162b4d2fd43d8a2b0b5fa76
harryborison920613 <149662036+harryborison920613@users.noreply.github.com>
2024-01-29 04:39:15 -0800

    marketplace page changed, buy function added to marketplace page

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -4,6 +4,7 @@ import MarketplaceJSON from "../Marketplace.json";
 import axios from "axios";
 import { useState } from "react";
 import { GetIpfsUrlFromPinata } from "../utils";
+import { Spin, Alert } from "antd";
 
 export default function Marketplace() {
   const sampleData = [
@@ -39,7 +40,6 @@ export default function Marketplace() {
     },
   ];
   const [data, updateData] = useState(sampleData);
-
   const [dataFetched, updateFetched] = useState(false);
 
   async function getAllNFTs() {
@@ -83,8 +83,22 @@ export default function Marketplace() {
   }
 
   if (!dataFetched) getAllNFTs();
+  if (dataFetched && !data.length)
+    return (
+      <div style={{ minHeight: "100vh" }}>
+        <Navbar></Navbar>
+        <div className="flex flex-col place-items-center mt-40">
+          <Spin tip="Loading..." size="large">
+            <div className="content" />
+          </Spin>
+          <div className="mt-10 md:text-xl lg:text-3xl font-bold text-white">
+            No items in marketplace
+          </div>
+        </div>
+      </div>
+    );
   return (
-    <div>
+    <div style={{ minHeight: "100vh" }}>
       <Navbar></Navbar>
       <div className="flex flex-col place-items-center mt-20">
         <div className="md:text-xl font-bold text-white">Top NFTs</div>
--- src/components/NFTTile.js
+++ src/components/NFTTile.js
@@ -1,33 +1,76 @@
 import axie from "../tile.jpeg";
-import {
-    BrowserRouter as Router,
-    Link,
-  } from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import { GetIpfsUrlFromPinata } from "../utils";
+import MarketplaceJSON from "../Marketplace.json";
 
-function NFTTile (data) {
-    const newTo = {
-        pathname:"/nftPage/"+data.data.tokenId
+function NFTTile(data) {
+  const [message, updateMessage] = useState("");
+
+  const newTo = {
+    pathname: "/nftPage/" + data.data.tokenId,
+  };
+
+  const IPFSUrl = GetIpfsUrlFromPinata(data.data.image);
+
+  async function buyNFT(tokenId, price) {
+    try {
+      const ethers = require("ethers");
+      //After adding your Hardhat network to your metamask, this code will get providers and signers
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      //Pull the deployed contract instance
+      let contract = new ethers.Contract(
+        MarketplaceJSON.address,
+        MarketplaceJSON.abi,
+        signer
+      );
+      const salePrice = ethers.utils.parseUnits(price, "ether");
+      updateMessage("Buying the NFT... Please Wait (Upto 5 mins)");
+      //run the executeSale function
+      let transaction = await contract.executeSale(tokenId, {
+        value: salePrice,
+      });
+      await transaction.wait();
+
+      alert("You successfully bought the NFT!");
+      updateMessage("");
+    } catch (e) {
+      alert("Upload Error" + e);
     }
+  }
 
-    const IPFSUrl = GetIpfsUrlFromPinata(data.data.image);
-
-    return (
-        <Link to={newTo}>
-        <div className="border-2 ml-12 mt-5 mb-12 flex flex-col items-center rounded-lg w-48 md:w-72 shadow-2xl">
-            <img src={IPFSUrl} alt="" className="w-72 h-80 rounded-lg object-cover" />
-            <div className= "text-white w-full p-2 bg-gradient-to-t from-[#454545] to-transparent rounded-lg pt-5 -mt-20">
-                <strong className="text-xl">{data.data.name}</strong>
-                <p className="display-inline">
-                    {data.data.description}
-                </p>
-                <p className="display-inline">
-                    {data.data.tokenId}
-                </p>
-            </div>
+  return (
+    <div className="border-2 rounded-xl ml-12 mt-5 mb-12 flex flex-col items-center w-48 md:w-72 shadow-2xl overflow-hidden">
+      <Link to={newTo}>
+        <img
+          src={IPFSUrl}
+          alt=""
+          className="w-72 h-80 object-cover"
+        />
+      </Link>
+      <div className="text-white w-full bg-white">
+        <div className="p-4">
+          <p style={{ height: "40px" }} className="text-xl font-semibold text-black">
+            {data.data.name}
+          </p>
+          <div style={{ overflow: "hidden" }}>
+            <p className="text-gray-400">{data.data.description}</p>
+          </div>
         </div>
-        </Link>
-    )
+        <div className="p-1 bg-black">
+          <p className="text-lg text-white">{data.data.price} ETH</p>
+          <button
+            className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+            onClick={() => buyNFT(data.data.tokenId, data.data.price)}
+          >
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NFTTile;
