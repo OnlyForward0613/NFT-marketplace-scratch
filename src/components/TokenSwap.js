6961b2c3e316297f464ba338dbeaf4728ffc9b56
harryborison920613 <149662036+harryborison920613@users.noreply.github.com>
2024-01-31 12:47:37 -0800

    getTokenList

--- src/components/TokenSwap.js
+++ src/components/TokenSwap.js
@@ -1,12 +1,35 @@
 import React, { useEffect, useState } from "react";
-import Modal from "../atoms/Modal";
+// import Modal from "../atoms/Modal";
+import { Modal } from "antd";
 
 export default function TokenSwap() {
   const [isDisable, setIsDisable] = useState(true);
-  let testData='';
+  const [tokenList, setTokenList] = useState();
+  const [tokenFirst, setTokenFirst] = useState("");
+  const [tokenSecond, setTokenSecond] = useState("");
+  const [isFirst, setIsFirst] = useState(false);
+  const [isSecond, setIsSecond] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const hideModal = () => {
+    setVisible(false);
+  };
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  async function getTokenList() {
+    console.log("initializing");
+    let response = await fetch("https://tokens.coingecko.com/uniswap/all.json");
+    let tokenListJSON = await response.json();
+    console.log("listing available tokens: ", tokenListJSON.tokens);
+    setTokenList(tokenListJSON.tokens);
+  }
+
   useEffect(() => {
     connect();
-    testData='testData';
+    getTokenList();
   }, []);
   function updateButton() {
     const ethereumButton = document.querySelector(".enableSwapButton");
@@ -41,21 +64,41 @@ export default function TokenSwap() {
     }
   }
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <div style={{ height: "70vh" }}>
-      <Modal isOpen={isModalOpen} onClose={closeModal} children={testData}>
-        <h1>Modal Content</h1>
-        <button onClick={closeModal}>Close Modal</button>
+      <Modal
+        title="Select a token"
+        open={visible}
+        onOk={hideModal}
+        onCancel={hideModal}
+        okText="Ok"
+        cancelText="Cancel"
+        okType="danger"
+        width={"20vw"}
+      >
+        <div>
+          {tokenList &&
+            tokenList.map((token) => {
+              return (
+                <div
+                  className="py-1 cursor-pointer"
+                  onClick={() => {
+                    if (isFirst) setTokenFirst(token);
+                    if (isSecond) setTokenSecond(token);
+                    hideModal();
+                  }}
+                  key={token.address}
+                >
+                  <img
+                    src={token.logoURI}
+                    key={token.decimals}
+                    className="inline"
+                  />
+                  <span className="ml-3">{token.symbol}</span>
+                </div>
+              );
+            })}
+        </div>
       </Modal>
       <div className="max-w-2xl mx-auto py-6 mt-20 sm:px-6 lg:px-8 bg-black text-white rounded-2xl">
         <div className="md:col-span-1">
@@ -64,12 +107,30 @@ export default function TokenSwap() {
             <div className="mt-5">
               <div className="grid grid-cols-1 gap-6">
                 <div className="col-span-6 sm:col-span-3 bg-gray-700 p-5 rounded-lg border-solid border-2 border-gray-500">
-                  <button
-                    className="text-md font-medium text-white w-[30%]"
-                    onClick={openModal}
-                  >
-                    SELECT A TOKEN
-                  </button>
+                  {tokenFirst ? (
+                    <div
+                      className="cursor-pointer text-md font-medium text-white w-[30%] bg-transparent inline-block"
+                      onClick={showModal}
+                    >
+                      <img
+                        src={tokenFirst.logoURI}
+                        key={tokenFirst.address}
+                        className="inline ml-6"
+                      />
+                      <span className="ml-3">{tokenFirst.symbol}</span>
+                    </div>
+                  ) : (
+                    <button
+                      className="text-md font-medium text-white w-[30%]"
+                      onClick={() => {
+                        setIsFirst(true);
+                        setIsSecond(false);
+                        showModal();
+                      }}
+                    >
+                      SELECT A TOKEN
+                    </button>
+                  )}
                   <input
                     type="text"
                     name="token1"
@@ -79,12 +140,30 @@ export default function TokenSwap() {
                   />
                 </div>
                 <div className="col-span-6 sm:col-span-3 bg-gray-700 p-5 rounded-lg border-solid border-2 border-gray-500">
-                  <button
-                    htmlFor="token2"
-                    className="text-md font-medium text-white w-[30%]"
-                  >
-                    SELECT A TOKEN
-                  </button>
+                  {tokenSecond ? (
+                    <div
+                      className="cursor-pointer text-md font-medium text-white w-[30%] bg-transparent inline-block"
+                      onClick={showModal}
+                    >
+                      <img
+                        src={tokenSecond.logoURI}
+                        key={tokenSecond.address}
+                        className="inline ml-6"
+                      />
+                      <span className="ml-3">{tokenSecond.symbol}</span>
+                    </div>
+                  ) : (
+                    <button
+                      className="text-md font-medium text-white w-[30%]"
+                      onClick={() => {
+                        setIsSecond(true);
+                        setIsFirst(false);
+                        showModal();
+                      }}
+                    >
+                      SELECT A TOKEN
+                    </button>
+                  )}
                   <input
                     type="text"
                     name="token2"
